b62c429767e9c04a288b8207bafa85ad40bfc69c
kkasper1 <karol.kasperek.stud@pw.edu.pl>
2022-12-12 08:38:44 +0100

    wypisywanie danych o dokumentach w bazie

--- src/main/frontend/src/find.js
+++ src/main/frontend/src/find.js
@@ -14,8 +14,34 @@ class Find extends React.Component{
         this.setState({value: event.target.value});
     }
 
+    load (url) {
+      return new Promise(async function (resolve, reject) {
+        const res = await fetch(url)
+        resolve(res.json())
+      })
+    }
+
     findDoc(event){
         alert('Podano taka nazwe dokumentu: ' + this.state.value);
+        if(this.state.value == ''){
+            const promise = this.load('http://localhost:8082/lawTexts');
+            promise.then((objects) => {
+                  for (const object of objects){
+                        alert('Informacje o dokumentach: \nID: ' + JSON.stringify(object.id) + '\nName: ' + JSON.stringify(object.name) + '\nUpload Date: ' + JSON.stringify(object.uploadDate) + '\nAccepted: ' + JSON.stringify(object.accepted) + '\nFile: ' + JSON.stringify(object.file) + '\nReferences: ' + JSON.stringify(object.references));
+                        }
+                  }
+            );
+        }
+        else
+        {
+            const promise = this.load(`localhost:8082/lawTexts/findByName/${this.state.value}`);
+                 promise.then((objects) => {
+                       for (const object of objects){
+                             alert('Informacje o dokumentach: \nID: ' + JSON.stringify(object.id) + '\nName: ' + JSON.stringify(object.name) + '\nUpload Date: ' + JSON.stringify(object.uploadDate) + '\nAccepted: ' + JSON.stringify(object.accepted) + '\nFile: ' + JSON.stringify(object.file) + '\nReferences: ' + JSON.stringify(object.references));
+                             }
+                       }
+                 );
+        }
         event.preventDefault();
     }
 
