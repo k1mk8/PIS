d774b19cf689450ab3e34689fcbc0d322ee4d6f4
Szymon Dyszewski <01158617@pw.edu.pl>
2023-01-05 19:50:39 +0100

    Oprawa wizualna frontu

--- src/main/frontend/src/components/addFile.js
+++ src/main/frontend/src/components/addFile.js
@@ -1,30 +1,95 @@
-import React from 'react';
-import '../styles/addFile.css'
+import React, { Component } from "react";
+import "../styles/addFile.css";
+import UploadService from "./upload-files.service";
 
-class Add extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            selectedFile: File,
-            setSelectedFile: File,
-            isFilePicked: Boolean,
-            setisFilePicked: Boolean,
-            isSelected: Boolean
-        }
-    }
-    changeHandler = (event) => {
-        this.setSelectedFile(event.target.files[0]);
-        this.isSelected = true;
-    }
-    render() {
-        return(
-            <div>
-                <p>Prosze załączyć plik i wcisnąć przycisk dodaj</p>
-                <input type="file" name="file" onChange={this.changeHandler}/>
-                <div>{'\n'}</div>
-                <button >Dodaj</button>
+export default class Add extends Component {
+  constructor(props) {
+    super(props);
+    this.selectFile = this.selectFile.bind(this);
+    this.upload = this.upload.bind(this);
+
+    this.state = {
+      selectedFiles: undefined,
+      currentFile: undefined,
+      progress: 0,
+      message: "",
+
+      fileInfos: [],
+    };
+  }
+  selectFile(event) {
+    this.setState({
+      selectedFiles: event.target.files,
+    });
+  }
+
+  upload() {
+    let currentFile = this.state.selectedFiles[0];
+
+    this.setState({
+      progress: 0,
+      currentFile: currentFile,
+    });
+
+    UploadService.upload(currentFile, (event) => {
+      this.setState({
+        progress: Math.round((100 * event.loaded) / event.total),
+      });
+    })
+      .then((files) => {
+        this.setState({
+          fileInfos: files.data,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          progress: 0,
+          message: "Could not upload the file!",
+          currentFile: undefined,
+        });
+      });
+
+    this.setState({
+      selectedFiles: undefined,
+    });
+  }
+
+  render() {
+    const {
+      selectedFiles,
+      currentFile,
+      progress,
+    } = this.state;
+
+    return (
+      <div>
+        {currentFile && (
+          <div className="progress">
+            <div
+              className="progress-bar progress-bar-info progress-bar-striped"
+              role="progressbar"
+              aria-valuenow={progress}
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style={{ width: progress + "%" }}
+            >
+              {progress}%
             </div>
-        );
-    }
+          </div>
+        )}
+
+        <label className="btn btn-default">
+          <input type="file" onChange={this.selectFile} />
+        </label>
+
+        <button
+          className="btn btn-success"
+          disabled={!selectedFiles}
+          onClick={this.upload}
+        >
+          Upload
+        </button>
+      </div>
+    );
+  }
 }
-export default Add;
